7580b7a876fd9d555d1c4fc8b5deaa37dec2c47f
mgalen007 <hyllusx@gmail.com>
2026-04-09 17:03:37 +0200

    Added OpenWeatherMap calling in get_weather

--- tools/get_weather.py
+++ tools/get_weather.py
@@ -1,23 +1,45 @@
+from dotenv import load_dotenv
+import os, requests
+load_dotenv()
 
 get_weather_schema = {
     "type": "function",
     "function": {
         "name": "get_weather",
-        "description": "Get the current weather for a city. Use this when the user asks about weather",
+        "description": "Get the current weather for a city. Use this when the user asks about weather, make sure to use the latitude and longitude of the city, not the name.",
         "parameters": {
             "type": "object",
             "properties": {
-                "city": {
+                "lat": {
                     "type": "string",
-                    "description": "The city name, e.g Kigali"
+                    "description": "The latitude of the city in context"
+                },
+                "lon": {
+                    "type": "string",
+                    "description": "The longitude of the city in context"
                 }
-            },
-            "required": ["city"]
+        },
+            "required": ["lat", "lon"]
         }
     }
 }
 
-def get_weather(city: str) -> None:
-    return f"Sunny in {city}, 24°C"
+def get_weather(lat: str, lon: str) -> str:
+    key = os.getenv("OPENWEATHER_API_KEY")
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={key}&units=metric"
+
+    response = requests.get(url)
+
+    if response.status_code == 429:
+        return "API exhausted, try again after some time"
+    elif not response.status_code == 200:
+        return "Error: failed to retrieve weather data"
+    else:
+        data = response.json()
+
+    description = data["weather"][0]["description"]
+    temp = data["main"]["temp"]
+
+    return f"Temperature: {temp}, description: {description}"
 
     
