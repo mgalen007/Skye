2fbacb0208286c08f13ec4299b170caa49acf537
mgalen007 <hyllusx@gmail.com>
2026-04-09 17:13:27 +0200

    Added a system prompt, for better seamless experience

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from agent.runner import run_agent
 
 while True:
-    prompt = input("You: ")
-    messages = [{"role": "user", "content": prompt}]
+    prompt = input("You -> ")
+    messages = [
+        { "role": "user", "content": prompt },
+        { 
+            "role": "system",
+            "content": '''You are a friendly AI assistant, sometimes when you call tools, you may not get a valid or expected output
+            make sure that you handle it gracefully, and explain the problem.'''
+        
+        }
+    ]
     result = run_agent(messages)
-    print(f"(model): {result}") 
+    print(f"Model -> {result}") 
